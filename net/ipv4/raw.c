bad858ff07a0d78381bbc733d4e8801b2ee15ee3
Mohamed Ghannam <simo.ghannam@gmail.com>
2017-12-10 03:50:58 +0000

    net: ipv4: fix for a race condition in raw_sendmsg
    commit 8f659a03a0ba9289b9aeb9b4470e6fb263d6f483 upstream.
    
    inet->hdrincl is racy, and could lead to uninitialized stack pointer
    usage, so its value should be read only once.
    
    Fixes: c008ba5bdc9f ("ipv4: Avoid reading user iov twice after raw_probe_proto_opt")
    Signed-off-by: Mohamed Ghannam <simo.ghannam@gmail.com>
    Reviewed-by: Eric Dumazet <edumazet@google.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    [bwh: Backported to 3.2:
     - flowi4 flags don't depend on hdrincl
     - Adjust context]
    Signed-off-by: Ben Hutchings <ben@decadent.org.uk>
    [aviraxp: Add back flowi4 flags changes]
    Signed-off-by: Ad!thya R <gh0strider.2k18.reborn@gmail.com>

--- net/ipv4/raw.c
+++ net/ipv4/raw.c
@@ -595,8 +595,12 @@ static int raw_sendmsg(struct kiocb *iocb, struct sock *sk, struct msghdr *msg,
 			   RT_SCOPE_UNIVERSE,
 			   hdrincl ? IPPROTO_RAW : sk->sk_protocol,
 			   inet_sk_flowi_flags(sk) | FLOWI_FLAG_CAN_SLEEP |
-			    (inet->hdrincl ? FLOWI_FLAG_KNOWN_NH : 0),
+			    (hdrincl ? FLOWI_FLAG_KNOWN_NH : 0),
 			   daddr, saddr, 0, 0, sk->sk_uid);
+
+	if (!hdrincl) {
+		rfv.iov = msg->msg_iov;
+		rfv.hlen = 0;
 
 		err = raw_probe_proto_opt(&rfv, &fl4);
 		if (err)
